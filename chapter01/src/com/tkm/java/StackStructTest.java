23d432b08c3b7a9c269338af9e0ea39444304869
刘德龙 <793854601@qq.com>
2022-11-08 18:00:45 +0800

    JVM体系

---
+++ chapter01/src/com/tkm/java/StackStructTest.java
@@ -0,0 +1,20 @@
+package com.tkm.java;
+
+public class StackStructTest {
+    public static void main(String[] args) {
+        /*
+            0: iconst_2
+            1: istore_1
+            2: iconst_3
+            3: istore_2
+            4: iload_1
+            5: iload_2
+            6: iadd
+            7: istore_3
+            8: return
+         */
+        int i = 2;
+        int j = 3;
+        int k = i + j;
+    }
+}
