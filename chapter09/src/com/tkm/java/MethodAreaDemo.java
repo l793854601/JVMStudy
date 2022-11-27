0f3be0cb98510a1c5f88df801f38dcf41dee53e7
l793854601 <793854601@qq.com>
2022-11-27 21:38:34 +0800

    方法区内存概述

--- chapter09/src/com/tkm/java/MethodAreaDemo.java
+++ chapter09/src/com/tkm/java/MethodAreaDemo.java
@@ -3,6 +3,12 @@ package com.tkm.java;
 /*
     测试设置方法区大小参数的默认值
     -Xms600m -Xmx600m
+
+    JDK7及以前：
+    -XX:PermSize=100m -XX:MaxPermSize=100m
+
+    JDK8及以后：
+    -XX:MetaspaceSize=100m -XX:MaxMetaspaceSize=100m
  */
 public class MethodAreaDemo {
     public static void main(String[] args) {
